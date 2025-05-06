30c5bf706aeed7b9518c5dc66b940b6af6c63034
Dhruv Kumar <dhrvua0786@gmail.com>
2025-05-06 21:53:01 +0530

    first

---
+++ Variables.java
@@ -0,0 +1,15 @@
+class Variables{
+
+    public static void main(String[] args){
+        int number = 10; // integer variable
+        double decimal = 20.5; // double variable
+        char letter = 'A'; // character variable
+        String text = "Hello, World!"; // string variable
+        boolean isTrue = true; // boolean variable
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + decimal);
+        System.out.println("Character: " + letter);
+        System.out.println("String: " + text);
+        System.out.println("Boolean: " + isTrue);
+    }
+}
