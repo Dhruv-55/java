f851ede092bbe229f1c16d4eec376ff6354022cc
Dhruv Kumar <dhrvua0786@gmail.com>
2025-05-07 22:12:08 +0530

    sum

---
+++ Methods.java
@@ -0,0 +1,9 @@
+class Methods{
+    static int add(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args){
+     int sum = add(5, 10);
+        System.out.println("The sum is: " + sum);          
+    }
+}
